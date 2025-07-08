f7a1bf3210c1e834174ad6142e9f1adcdbc28f30
Brijunas <32673235+Brijunas@users.noreply.github.com>
2025-07-09 02:10:06 +0300

    setup mongo in docker-compose

--- Infra/DependencyInjection.cs
+++ Infra/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Core.Interfaces.External;
+﻿using App.Interfaces;
+using Core.Interfaces.External;
+using Infra.Repositories;
 using Infra.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +12,7 @@ namespace Infra
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddHttpClient<IOpenWeatherClient, OpenWeatherClient>();
+            services.AddScoped<IOpenWeatherGetCurrentRepository, OpenWeatherGetCurrentRepository>();
             return services;
         }
     }
